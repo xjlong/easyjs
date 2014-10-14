16a242aaf3602327e86eedca31cd81856623c544
xjlong <1102212771@qq.com>
2014-10-14 16:37:11 +0800

    EAsjs

---
+++ easy.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    var path, i, ln, scriptSrc, match,
+    scripts = document.getElementsByTagName('script');
+
+    for (i = 0, ln = scripts.length; i < ln; i++) {
+        scriptSrc = scripts[i].src;
+
+        match = scriptSrc.match(/easy\.js$/);
+
+        if (match) {
+            path = scriptSrc.substring(0, scriptSrc.length - match[0].length);
+            break;
+        }
+    }
+
+    document.write('<link href="' + path + 'resources/css/all-mini.css" rel="stylesheet" type="text/css" />');
+    document.write('<link href="' + path + 'theme/deepblue/css/all-mini.css" rel="stylesheet" type="text/css" />');
+    
+    document.write('<script type="text/javascript" src="' + path + 'Easy-base-mini.js"></script>');
+    document.write('<script type="text/javascript" src="' + path + 'Easy-ui-mini.js"></script>');
+    if (navigator.userAgent.indexOf("MSIE 6") > -1) {
+        document.write('<link href="' + path + 'theme/ie6hack/css/all.css" rel="stylesheet" type="text/css" />');
+    }
+})();
---
+++ upload.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using Jade.Core.Util;
+
+namespace Aby.Easy
+{
+    public partial class upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.ContentType = "text/html";
+            if (IsGetFile)
+            {
+                List<FileModel> list = new List<FileModel>();
+                var ds = Directory.GetDirectories(path);
+                foreach (string d in ds)
+                {
+                    list.Add(new FileModel
+                    {
+                        fileName = d.Substring(d.LastIndexOf("\\")+1),
+                        isFolder = true
+                    });
+                }
+                var fs = Directory.GetFiles(path);
+                foreach (string d in fs)
+                {
+                    list.Add(new FileModel
+                    {
+                        fileName = d.Substring(d.LastIndexOf("\\") + 1),
+                        isFolder = false
+                    });
+                }
+                Response.Write(Jade.Core.JSON.JsonConvert.SerializeObject(list));
+            }
+            else if (IsRemoveFile && Files!=null)
+            {
+                Files.ForEach(o =>
+                {
+                    var ps = o.Split(';');
+                    if (ps[1] == "true")
+                    {
+                        Directory.Delete(path + "/" + ps[0], true);
+                    }
+                    else
+                    {
+                        File.Delete(path + "/" + ps[0]);
+                    }
+                });
+                Response.Write(Jade.Core.JSON.JsonConvert.SerializeObject(new Jade.Core.Entity.JsonReturn()));
+            }
+            else if (IsUploadFile) {
+                var f = Request.Files[0];
+                if (f != null && f.ContentLength > 0)
+                {
+                    string fn = Path.GetFileName(f.FileName);
+                    if (File.Exists(path + "/" + fn))
+                    {
+                        fn = fn.Substring(0, fn.LastIndexOf('.')) + "(" + DateTime.Now.Ticks + ")" + Path.GetExtension(fn);
+                    }
+                    f.SaveAs(path + "/" + fn);
+                    Response.Write(Jade.Core.JSON.JsonConvert.SerializeObject(new Jade.Core.Entity.JsonReturn { exception = false }));
+                }
+                else
+                {
+                    Response.Write(Jade.Core.JSON.JsonConvert.SerializeObject(new Jade.Core.Entity.JsonReturn
+                    {
+                        exception = true,
+                        errors = "文件为空!"
+                    }));
+                }
+            }
+        }
+
+        public List<string> Files
+        {
+            get
+            {
+                var f = Request["files"];
+                if (!string.IsNullOrEmpty(f))
+                {
+                    return (List<string>)Convert.ChangeType(f, typeof(List<string>));
+                }
+                return null;
+            }
+        }
+
+        public string path
+        {
+            get {
+                string p = Request["basePath"];
+                if (!string.IsNullOrEmpty(p))
+                {
+                    return Server.MapPath(p);
+                }
+                else {
+                    return null;
+                }
+            }
+        }
+
+
+        public bool IsUploadFile
+        {
+            get
+            {
+                return Request["action"].ToLower() == "uploadfile";
+            }
+        }
+        public bool IsGetFile
+        {
+            get
+            {
+                return Request["action"].ToLower() == "getfile";
+            }
+        }
+        public bool IsRemoveFile
+        {
+            get
+            {
+                return Request["action"].ToLower() == "removefile";
+            }
+        }
+
+    }
+
+    public class FileModel {
+        public string fileName;
+        public bool isFolder;
+    }
+
+}
